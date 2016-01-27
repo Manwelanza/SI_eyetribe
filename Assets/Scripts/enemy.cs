e7dbe0814c45c097ee7eb4514d06e44422e4ecda
Manuel <Manwelanza@gmail.com>
2016-01-27 13:04:29 +0000

    add points done

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+    public int points = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "PlayerBullet")
+        {
+            updatePoints.add(points);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public GameObject shooter;
+    public float delayShoot = 0.5f;
+    public float delayStart = 0f;
+
+    private float timerShoot = 0f;
+    private float timerStart = 0f; 
+
+	// Use this for initialization
+	void Start () {
+        timerShoot = 1 + delayShoot;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (timerStart > delayStart)
+        {
+            if (timerShoot > delayShoot)
+            {
+                Instantiate(bullet, shooter.transform.position, shooter.transform.rotation);
+                timerShoot = 0f;
+            }
+            timerShoot += Time.deltaTime;
+        }
+        else
+        {
+            timerStart += Time.deltaTime;
+        }
+	}
+}
