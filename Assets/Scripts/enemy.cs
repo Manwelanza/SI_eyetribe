dff41bbdeeef3ad91f3166d989cb1e814b90b4ac
Manuel <Manwelanza@gmail.com>
2016-01-29 11:44:30 +0000

    move player with mouse

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -4,23 +4,74 @@ using System.Collections;
 public class enemy : MonoBehaviour {
 
     public int points = 10;
+    public float speed = 10f;
+    
+    private static int n_enemies = 0;
+    private static bool right = true;
+    private static bool secure = false;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        n_enemies++;
+        speed = 50f;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        int aux;
+        if (right)
+        {
+            aux = 1;
+        }
+        else
+        {
+            aux = -1;
+        }
+
+        transform.Translate(aux * speed * Time.deltaTime, 0, 0);
+        if (secure)
+        {
+            secure = false;
+        }
 	}
+
+    public void  moveDown ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].transform.Translate(0, -5, 0);
+            //enemies[i].GetComponent<Collider>().transform.Translate(0, -5, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.collider.tag == "wall")
+        {
+            if (!secure)
+            {
+                secure = true;
+                if (right)
+                {
+                    right = false;
+                }
+                else
+                {
+                    right = true;
+                }
+                moveDown();
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "PlayerBullet")
         {
             updatePoints.add(points);
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); 
         }
     }
+
 }
---
+++ Assets/Scripts/movePlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private float diference;
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        diference = 76.44f - 57.5f + 11;
+        controller = GetComponent<CharacterController> ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 positionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 positionPlayer = transform.position - new Vector3(diference, 0, 0);
+        Vector3 movement = new Vector3 (speed * Time.deltaTime, 0, 0);
+
+        if (positionMouse.x - positionPlayer.x > 1)
+        {
+            // Move Right
+            controller.Move(movement);
+        }
+        else if (positionMouse.x - positionPlayer.x < -1)
+        {
+            // Move Left
+            controller.Move(-1 * movement);
+        }
+
+       //Debug.Log("Mouse -->" + positionMouse.x);
+       //Debug.Log("Player -->" + positionPlayer.x);
+      
+
+    }
+}
